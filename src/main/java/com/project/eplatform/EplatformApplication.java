e20d32bdba0ba57006198f0a6f4c2b452563eec3
Nguyen Vo <dashzero123@gmail.com>
2023-04-09 19:53:58 +0700

    new file:   .gitignore 	new file:   .jpb/persistence-units.xml 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/project/eplatform/EplatformApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/project/eplatform/EplatformApplicationTests.java

---
+++ src/main/java/com/project/eplatform/EplatformApplication.java
@@ -0,0 +1,13 @@
+package com.project.eplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/eplatform/EplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
