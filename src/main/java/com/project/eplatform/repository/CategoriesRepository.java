f388ebafcdf276127a53bcb653383ad31c2de632
Nguyen Vo <dashzero123@gmail.com>
2023-05-15 20:55:51 +0700

    Added 2 more models, added some Product methods

---
+++ src/main/java/com/project/eplatform/repository/CategoriesRepository.java
@@ -0,0 +1,2 @@
+package com.project.eplatform.repository;public interface CategoriesRepository {
+}
---
+++ src/main/java/com/project/eplatform/service/implementation/ProductServiceImpl.java
@@ -0,0 +1,2 @@
+package com.project.eplatform.service.implementation;public class ProductServiceImpl {
+}
